ae61a56505b552d42444e0e017ce8530fc0fb53f
Park Seong-beom <parkgds@gmail.com>
2018-09-17 00:34:59 +0900

    feat: 카드 swipe 제스처 구현  (#5)

--- src/screens/Main/Home/index.js
+++ src/screens/Main/Home/index.js
@@ -6,7 +6,10 @@
 import React from 'react';
 import {
   StyleSheet,
+  View,
   ScrollView,
+  Animated,
+  PanResponder,
 } from 'react-native';
 import uuidvl from 'uuid';
 import UserCards from './components/UserCards';
@@ -15,7 +18,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
-    padding: 15,
+    // padding: 15,
   },
 });
 
@@ -28,6 +31,7 @@ class Home extends React.Component {
     super(props);
 
     this.state = {
+      cardsPan: new Animated.ValueXY(),
       cards: [
         {
           users: [
@@ -59,15 +63,81 @@ class Home extends React.Component {
         },
       ],
     };
+
+    /**
+     * 터치 제스처를 제어한다.
+     * 각 property의 parameters는 React Native 공식 문서를 참고:
+     * https://facebook.github.io/react-native/docs/panresponder
+     */
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onStartShouldSetPanResponderCapture: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+
+      // 터치가 시작될 때.
+      onPanResponderGrant: () => { },
+
+      // 터치 이후 움직일 때. (드래그)
+      // TODO: 카드를 움직일 때 scrollView가 스크롤되지 않도록 막기.
+      onPanResponderMove: (evt, gestureState) => {
+        const { cardsPan } = this.state;
+
+        if (gestureState.dx < -20 || gestureState.dx > 20) {
+          cardsPan.setValue(
+            {
+              x: gestureState.dx,
+              y: cardsPan.y,
+            },
+          );
+        }
+      },
+      onPanResponderTerminationRequest: () => true,
+
+      // 터치가 끝날 때. (화면 상 어디에도 제스처가 없을 때.)
+      onPanResponderRelease: (evt, gestureState) => {
+        const { cardsPan } = this.state;
+
+        if (gestureState.dx > -150 && gestureState.dx < 150) {
+          cardsPan.setValue(
+            {
+              x: 0,
+              y: cardsPan.y,
+            },
+          );
+        }
+      },
+
+      // 다른 component가 responder가 될 때.
+      onPanResponderTerminate: () => { },
+
+      // child component에서 responder의 사용을 막는다.
+      onShouldBlockNativeResponder: () => {
+        return true;
+      },
+    });
   }
 
   render() {
-    const { cards } = this.state;
+    const { cardsPan, cards } = this.state;
 
     return (
-      <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-        <UserCards users={cards[0].users} />
-      </ScrollView>
+      <View style={styles.container}>
+        <ScrollView
+          {...this.panResponder.panHandlers}
+          showsVerticalScrollIndicator={false}
+        >
+          <Animated.View
+            style={{
+              transform: [
+                { translateX: cardsPan.x },
+              ],
+            }}
+          >
+            <UserCards users={cards[0].users} />
+          </Animated.View>
+        </ScrollView>
+      </View>
     );
   }
 }
