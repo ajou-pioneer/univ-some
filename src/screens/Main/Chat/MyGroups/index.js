5b3c400be3fbf38d68d2180ad6d44eef3ac79cfc
ParkSB <parkgds@gmail.com>
2018-09-22 18:45:42 +0900

    feat: 내 그룹 스크린 구현

--- src/screens/Main/Chat/MyGroups/index.js
+++ src/screens/Main/Chat/MyGroups/index.js
@@ -6,14 +6,39 @@ import React from 'react';
 import {
   StyleSheet,
   View,
+  ScrollView,
+  Image,
+  Text,
 } from 'react-native';
 import PropTypes from 'prop-types';
+import uuidvl from 'uuid';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
-    padding: 15,
+  },
+  groupContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#DFDFDF',
+    flexDirection: 'row',
+    paddingTop: 15,
+    paddingBottom: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  memberContainer: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  memberImage: {
+    height: 55,
+    width: 55,
+  },
+  memberName: {
+    marginTop: 5,
   },
 });
 
@@ -28,9 +53,76 @@ class MyGroups extends React.Component {
     }).isRequired,
   };
 
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userGroups: [
+        {
+          id: uuidvl(),
+          members: [
+            {
+              id: uuidvl(),
+              avatar: 'https://i.imgur.com/dbh6umy.jpg',
+              name: '김솨솨',
+            }, {
+              id: uuidvl(),
+              avatar: 'https://i.imgur.com/TqhmG2S.jpg',
+              name: '박솨솨',
+            }, {
+              id: uuidvl(),
+              avatar: 'https://i.imgur.com/b6y3QR0.jpg',
+              name: '최솨솨',
+            },
+          ],
+        }, {
+          id: uuidvl(),
+          members: [
+            {
+              id: uuidvl(),
+              avatar: 'https://i.imgur.com/dbh6umy.jpg',
+              name: '김뫄뫄',
+            }, {
+              id: uuidvl(),
+              avatar: 'https://i.imgur.com/TqhmG2S.jpg',
+              name: '박뫄뫄',
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  renderUserGroup = (group) => {
+    const listItems = group.members.map((member) => {
+      return (
+        <View style={styles.memberContainer} key={member.id}>
+          <Image
+            style={styles.memberImage}
+            source={{ uri: member.avatar }}
+            resizeMode="cover"
+            borderRadius={55}
+          />
+          <Text style={styles.memberName}>{member.name}</Text>
+        </View>
+      );
+    });
+
     return (
-      <View style={styles.container} />
+      <View style={styles.groupContainer} key={group.id}>
+        {listItems}
+      </View>
+    );
+  }
+
+  render() {
+    const { userGroups } = this.state;
+
+    return (
+      <ScrollView style={styles.container}>
+        {userGroups.map((group) => {
+          return this.renderUserGroup(group);
+        })}
+      </ScrollView>
     );
   }
 }
