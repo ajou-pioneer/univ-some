6187aef15813a4b940baf95bad73c04552d4470d
Park Seong-beom <parkgds@gmail.com>
2018-10-26 21:15:37 +0900

    feat: 그룹 정보 스크린 베이스 구현 (#21)
    * feat: 탐색 탭에서 그룹 정보 스크린 이동 구현
    * refactor: 그룹 정보 스크린에서 불필요한 옵션 제거
    * fix: 채팅 타이틀 수정

---
+++ src/screens/Main/Chat/Search/GroupInfo.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Park Seong-beom on 2018.8
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    padding: 15,
+  },
+});
+
+class GroupInfo extends React.Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func.isRequired,
+    }).isRequired,
+  };
+
+  render() {
+    const { navigation } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight
+          onPress={() => navigation.goBack()}
+        >
+          <Text>go back</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+export default GroupInfo;
